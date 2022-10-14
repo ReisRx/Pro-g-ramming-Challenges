16d0ef0a36fed8bb6dfc0f19828b9d27fc87be60
Rafael Reis <dasmen12@gmail.com>
2022-10-14 19:26:48 +0100

    Kept Aspect Ratio of image and get string from stdin safely

--- ImageToASCII_18/imageToASCII.c
+++ ImageToASCII_18/imageToASCII.c
@@ -5,22 +5,24 @@
 #include "libs/stb_image.h"
 #include "libs/stb_image_resize.h"
 
-#define WIDTH 80
-#define HEIGHT 80
+#define HEIGHT 198 // Vertical resolution
+
 #define DENSITY "@#WN$9876543210?!abc;:+=-,._                         "
-// #define DENSITY "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,^`"
+// #define DENSITY "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,^` "
 // #define DENSITY "@%#*+=-:. "
 
 int main(void) {
     // Get image name
-    char filename[50];
+    char filename[64];
     while(1) {
         printf("Enter the image file name: (png or jpg) ");
-        gets(filename);
+        fgets(filename, sizeof(filename), stdin);
         if(strstr(filename, ".png") == NULL && strstr(filename, ".jpg") == NULL) {
             printf("Invalid file type. Please enter a png or jpg file.\n");
         }
         else {
+            // Replace newline with null terminator
+            filename[strlen(filename) - 1] = '\0';
             break;
         }
     }
@@ -29,13 +31,16 @@ int main(void) {
     int width, height, channels;
     unsigned char *img = stbi_load(filename, &width, &height, &channels, 0);
     if (img == NULL) {
-        printf("Error loading image\n");
+        printf("Error loading image %s\n", filename);
         return 1;
     }
 
+    // Calculate new Aspect Ratio (It is added 1:2 to the AR because of spacing)
+    float aspectRatio = (float) width * (float) 2.0 / (float) height;
+
     // Resize image
-    int new_width = WIDTH;
     int new_height = HEIGHT;
+    int new_width = HEIGHT * aspectRatio;
     unsigned char *resized_img = malloc(new_width * new_height * channels);
     stbir_resize_uint8(img, width, height, 0, resized_img, new_width, new_height, 0, channels);
 
@@ -45,7 +50,7 @@ int main(void) {
     for(unsigned char *p = resized_img; p != resized_img + img_size; p += channels) {
         unsigned char gray = (p[0] + p[1] + p[2]) / 3;
         unsigned char index = gray * (strlen(DENSITY) - 1) / 255;
-        printf("%c ", DENSITY[index]);
+        printf("%c", DENSITY[index]);
         if(++counter % new_width == 0) {
             printf("\n");
         }
